fac40c326b34c14be504aa24d9d63a1966679f43
ChauSu36 <lesuchaule2020@gmail.com>
2026-07-14 19:06:49 +0700

    add sudo for docker in file deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ echo "===================================================="
 echo "User đang thực hiện: $(whoami)"
 echo "Hostname của server: $(hostname)"
 echo "Thời gian deploy: $(date)"
-
+echo ""
 
 echo "===================================================="
-echo "2. DI CHUYEB VÀO THƯ MUC DU ÁN"
+echo "2. DI CHUYEN VÀO THƯ MUC DU ÁN"
 echo "===================================================="
 cd ~/chausu-compose-web || { echo "ERORR, KHONG TIM THAY THU MUC"; exit 1; }
-
+echo ""
 
 echo "===================================================="
 echo "3. BUILD LAI HỆ THỐNG IMAGE"
@@ -24,24 +24,24 @@ echo "===================================================="
 git pull origin main
 
 # Tiến hành build lại các service
-docker compose build
+sudo docker compose build
 
 echo "===================================================="
 echo "4. KHOI ĐONG LAI STACK BANG DOCKER COMPOSE"
 echo "===================================================="
-docker compose down
-docker compose up -d
+sudo docker compose down
+sudo docker compose up -d
 
 echo "===================================================="
 echo "5. HIỂN THỊ TRẠNG THÁI CONTAINER SAU DEPLOY"
 echo "===================================================="
-docker compose ps
+sudo docker compose ps
 
 echo "===================================================="
 echo "6. IN 40 DÒNG LOG GẦN NHẤT CỦA HỆ THỐNG"
 echo "===================================================="
 # In log của cụm dịch vụ để kiểm tra lỗi khởi động
-docker compose logs --tail=40
+sudo docker compose logs --tail=40
 echo "===================================================="
 echo " QUY TRÌNH DEPLOY HOÀN TẤT!"
 echo "===================================================="
